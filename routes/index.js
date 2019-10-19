a5f066e45ba3348da3070124e7009fcc92cda34a
DickensJuma <dickensjuma13@gmail.com>
2019-10-19 21:42:44 +0300

    update sendgrid api key

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 require('dotenv').config();
 const sgMail = require('@sendgrid/mail');
+var nodemailer = require("nodemailer");
 
 router.get('/', (req, res) =>
     res.render('home'));
@@ -14,27 +15,30 @@ router.get('/', (req, res) =>
     router.get('/wishlist', (req, res) =>
     res.render('wishlist'));
 
-
+   
 router.post('/subscribe', (req, res) => {
     sgMail.setApiKey(process.env.API_KEY);
     const msg = {
-        to: 'silasdianga4@gmail.com',
+        to: 'dickensjuma13@gmail.com',
         from: '[Kadianga Electrical Limited]',
         subject: 'Subscribtion request',
         html: `<br>Email: ${req.body.email}<br>`,
     };
-    sgMail.send(msg);
-    res.status(200)
-    console.log("sucess");
-    req.flash('success_msg', 'Subscribtion request sucessfully!');
-    res.redirect("/")
+    sgMail.send(msg,function(err,json){
+        if(err){ return console.error(err);}
+        console.log(json);
+        console.log("sucess");
+        req.flash('success_msg', 'Subscribtion request sucessfully!');
+        res.redirect("/")
+    });
+    
 
 });
 router.post('/quote', (req, res) => {
     sgMail.setApiKey(process.env.API_KEY);
     const msg = {
-        to: 'silasdianga4@gmail.com',
-        from: 'orders@kadianga.com',
+        to: 'dickensjuma13@gmail.com',
+        from: '[Kadianga Electrical Limited]',
         subject: 'Orders',
         html: `<strong>Name: ${req.body.name} <br>Email: ${req.body.email}<br>
         Phone No: ${req.body.phone}<br><br>${req.body.quote}</strong>`,
